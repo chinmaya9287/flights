0a8c9a38f0d73f24aeb2b2b2ab06b268e60cd6ab
Justin Muller <wickedjust@hotmail.com>
2014-04-21 10:01:25 +1000

    Adding stylus to repo

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,24 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        stylus: {
+            compile: {
+                options: {
+                    paths: ['js/**/*.styl']
+//                    urlfunc: 'embedurl', // use embedurl('test.png') in our code to trigger Data URI embedding
+//                    use: [
+//                        require('fluidity') // use stylus plugin at compile time
+//                    ],
+//                    import: [      //  @import 'foo', 'bar/moo', etc. into every .styl file
+//                        'foo',       //  that is compiled. These might be findable based on values you gave
+//                        'bar/moo'    //  to `paths`, or a plugin you added under `use`
+//                    ]
+                },
+                files: {
+                    'build/main.css': ['js/**/*.styl']
+                }
+            }
+        },
         jslint: {
             client: {
                 src: [
@@ -16,10 +34,20 @@ module.exports = function(grunt) {
                     junit: 'out/client-junit.xml'
                 }
             }
+        },
+        karma: {
+            unit: {
+                frameworks: ['mocha'],
+                options: {
+                    files: ['test/**/*.js']
+                }
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.loadNpmTasks('grunt-jslint');
+    grunt.loadNpmTasks('grunt-karma');
 
-    grunt.registerTask('default', ['jslint']);
+    grunt.registerTask('default', ['jslint', 'karma', 'stylus']);
 };
